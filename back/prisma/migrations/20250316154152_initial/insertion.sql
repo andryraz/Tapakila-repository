171daf1a7e14a2750ac8e4d3ba5ce7fc84c3cc16
Tsitohaina <hei.voahary@gmail.com>
2025-04-03 15:41:13 +0300

    refactor(react_admin): delete unusual files and add some insertions on database

---
+++ back/prisma/migrations/20250316154152_initial/insertion.sql
@@ -0,0 +1,36 @@
+INSERT INTO "Evenement" (id, titre, "description", categorie, "image", "dateHeure", lieu, statut, "organisateurId") VALUES
+(5, 'Coldplay - Music of the Spheres Tour', 
+ 'Coldplay revient avec sa tournee mondiale "Music of the Spheres Tour" et fera escale a Paris pour un concert inoubliable.', 
+ 'CONCERT - POP ROCK', 
+ 'https://www.ticketmaster.fr/statics/vignettes/n_coldplay_p.webp', 
+ '2025-07-10 21:00:00', 
+ 'Stade de France, Paris', 
+ 'PUBLIE', 11),
+
+(6, 'Tomorrowland 2025', 
+ 'Le plus grand festival de musique electronique au monde revient avec les meilleurs DJs pour une edition explosive.', 
+ 'FESTIVAL - MUSIQUE ELECTRONIQUE', 
+ 'https://www.tomorrowland.com/src/Frontend/Themes/tomorrowland/Core/Layout/images/share.jpg', 
+ '2025-07-18 14:00:00', 
+ 'Boom, Belgique', 
+ 'PUBLIE', 12),
+
+(7, 'Jazz a Vienne 2025', 
+ 'Le festival international de jazz vous donne rendez-vous pour une nouvelle edition avec des artistes de renom.', 
+ 'CONCERT - JAZZ', 
+ 'https://www.jazzavienne.com/sites/default/files/styles/social/public/2024-01/JAZZ_A_VIENNE_2024_SQUARE.jpg', 
+ '2025-06-28 20:00:00', 
+ 'Theatre Antique de Vienne, France', 
+ 'PUBLIE', 2
+),
+
+(8, 'Metallica - M72 World Tour', 
+ 'Metallica enflamme la scene avec leur tournee M72, une experience inoubliable pour tous les fans de metal.', 
+ 'CONCERT - METAL', 
+ 'https://www.ticketmaster.fr/statics/vignettes/n_metallica_p.webp', 
+ '2025-09-15 20:00:00', 
+ 'Accor Arena, Paris', 
+ 'PUBLIE', 12
+);
+
+
--- back/prisma/seed.ts
+++ back/prisma/seed.ts
@@ -4,18 +4,32 @@ const prisma = new PrismaClient();
 
 async function main() {
  
-    const organisateur = await prisma.utilisateur.create({
-        data: {
-          nom: "Chris Patt",
-          email: "patt@gmail.com",
-          motDePasse: "password",
-          role: Role.ORGANISATEUR,
-        },
-      });
+    // const organisateur = await prisma.utilisateur.create({
+    //     data: {
+    //       nom: "Chris Patt",
+    //       email: "patt@gmail.com",
+    //       motDePasse: "password",
+    //       role: Role.ORGANISATEUR,
+    //     },
+    //   });
 
-  const event1 = await prisma.evenement.create({
+  // const event1 = await prisma.evenement.create({
+  //   data: {
+  //     titre: "Indochine",
+  //     description: "Indochine a recemment présenté leur nouvel album 'Babel Babel' lors d'un concert à huis clos devant 300 fans privilégiés dans un lieu secret, près de Paris. Suite à ce concert exceptionnel, Indochine revient en 2025 avec l'Arena Tour, une tournée exceptionnelle en indoor et en arenas, passant par 17 villes en France, Belgique et Suisse, avec deux concerts à l’Accor Arena de Paris les vendredi 20 et samedi 21 juin 2025.",
+  //     categorie: "CONCERT - POP ROCK FOLK",
+  //     image: "https://www.ticketmaster.fr/statics/vignettes/n_indochine_p.webp",
+  //     dateHeure: new Date("2025-10-24T20:00:00Z"),
+  //     lieu: "ZENITH TOULOUSE METROPOLE",
+  //     statut: StatutEvenement.PUBLIE,
+  //     organisateurId: 1, 
+  //   },
+  // });
+
+
+    const event1 = await prisma.evenement.create({
     data: {
-      titre: "Indochine",
+      titre: "TARIKA BE",
       description: "Indochine a recemment présenté leur nouvel album 'Babel Babel' lors d'un concert à huis clos devant 300 fans privilégiés dans un lieu secret, près de Paris. Suite à ce concert exceptionnel, Indochine revient en 2025 avec l'Arena Tour, une tournée exceptionnelle en indoor et en arenas, passant par 17 villes en France, Belgique et Suisse, avec deux concerts à l’Accor Arena de Paris les vendredi 20 et samedi 21 juin 2025.",
       categorie: "CONCERT - POP ROCK FOLK",
       image: "https://www.ticketmaster.fr/statics/vignettes/n_indochine_p.webp",
@@ -25,6 +39,7 @@ async function main() {
       organisateurId: 1, 
     },
   });
+
 
   const event2 = await prisma.evenement.create({
     data: {
